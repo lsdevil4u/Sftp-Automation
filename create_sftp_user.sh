440c23ee7ee78378987b2976517439059e6e36c1
Ashutosh Kaushik <jecrchai@gmail.com>
2024-02-25 21:15:37 +0530

    role added

--- create_sftp_user.sh
+++ create_sftp_user.sh
@@ -3,8 +3,12 @@
 # Input parameters
 username=$1
 
+# Replace 'your-iam-role-arn' with the actual ARN of your IAM role
+iam_role_arn="arn:aws:iam::660262893273:role/sftp-role"
+
+
 # Create SFTP user
-aws transfer create-user --server-id s-efd3d35baf264f6da --user-name $username
+aws transfer create-user --server-id s-efd3d35baf264f6da --user-name $username --role $iam_role_arn
 
 # Get public SSH key
 ssh_key=$(aws transfer describe-user --server-id s-efd3d35baf264f6da --user-name $username --query 'User.SshPublicKeys[0]' --output text)
