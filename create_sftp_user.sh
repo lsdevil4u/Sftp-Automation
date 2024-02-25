feee7a77d3aa30bb077a64d70d9efa84b1ac102b
Ashutosh Kaushik <jecrchai@gmail.com>
2024-02-25 21:03:26 +0530

    bucket name and sftp servers id added

--- create_sftp_user.sh
+++ create_sftp_user.sh
@@ -4,11 +4,11 @@
 username=$1
 
 # Create SFTP user
-aws transfer create-user --server-id your-server-id --user-name $username
+aws transfer create-user --server-id s-efd3d35baf264f6da --user-name $username
 
 # Get public SSH key
-ssh_key=$(aws transfer describe-user --server-id your-server-id --user-name $username --query 'User.SshPublicKeys[0]' --output text)
+ssh_key=$(aws transfer describe-user --server-id s-efd3d35baf264f6da --user-name $username --query 'User.SshPublicKeys[0]' --output text)
 
 # Upload public SSH key to S3 bucket
 echo "$ssh_key" > /tmp/$username.pub
-aws s3 cp /tmp/$username.pub s3://your-s3-bucket/$username.pub
+aws s3 cp /tmp/$username.pub s3://s3glacier-test1-dest/$username.pub
